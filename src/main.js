6e6547b7a91547236b395fcba4d4723f4b3a41cd
Jason Bejot <jasonbejot@gmail.com>
2021-06-27 11:37:52 -0700

    added google analytics and built for production

--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueGtag from 'vue-gtag';
 
-Vue.config.productionTip = false
+//Vue.config.productionTip = false
+
+Vue.use(VueGtag, {
+  config: { id: 'UA-318678-4' }
+});
 
 new Vue({
   render: h => h(App),
